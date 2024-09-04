363a29630aa870292d3815acecf1cc23d9e65014
Виктория Кузнецова <viktoria@Viktorias-MacBook-Pro.local>
2024-09-04 13:08:05 +0300

    fix the mistake in the start_thrd

--- part1/courseinfo/app/src/App.jsx
+++ part1/courseinfo/app/src/App.jsx
@@ -28,7 +28,7 @@ const Total = (props) => {
 const App = () => {
 	// const-definitions
 	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
+  const part1 = 'Fundamentals of React'
 	const exercises1 = 10
 	const part2 = 'Using props to pass data'
 	const exercises2 = 7
