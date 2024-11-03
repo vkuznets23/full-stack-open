c532590e7dd01df17130eeb7772351e8ed26bdb1
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-11-03 20:07:51 +0200

    final task with refactoring

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Form from './components/form.js'
+import Filter from './components/filter.js'
+import Numbers from './components/numbers.js'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -8,62 +11,21 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
 
-  const addPerson = (e) => {
-    e.preventDefault();
-    const newPerson = {name: newName, number: newNumber};
-    
-    if (!newName || !newNumber || !isFinite(newNumber)) {
-      alert("Both name and phone number must be provided.");
-      return;
-    }
-    const exist = persons.some(person => person.name === newPerson.name)
-    if (!exist) {
-      setPersons(persons.concat(newPerson));
-      setNewName('');
-      setNewNumber('');
-    }
-    else {
-      alert(`${newName} is already added to phonebook`)
-    }
-  }
-
-  const filterPersons = persons.filter(word => 
-    word.name.toLowerCase().includes(search.toLowerCase()) ||
-    word.number.includes(search)
-  );
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input 
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}/>
-      </div>
-      <h2>Add new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          <div>
-            name: <input 
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}/>
-          </div>
-          <div>
-            phone: <input 
-              value={newNumber}
-              onChange={(e) => setNewNumber(e.target.value)}/>
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {filterPersons.map((person, id) => (
-          <div key={id}> {person.name} {person.number}</div>
-        ))}
-      </div>
+      <Filter search={search} setSearch={setSearch} />
+      <h3>Add new</h3>
+      <Form 
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        persons={persons}
+        setPersons={setPersons}
+      />
+      <h3>Numbers</h3>
+      <Numbers persons={persons} search={search}/>
     </div>
   )
 }
