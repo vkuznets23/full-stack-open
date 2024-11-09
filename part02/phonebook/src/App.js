4d74742694892a7177c7855310281167e1633a98
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-11-09 20:39:39 +0200

    2.16 error messages

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -3,6 +3,7 @@ import Form from './components/form.js'
 import Filter from './components/filter.js'
 import Numbers from './components/numbers.js'
 import personsServices from './services/persons.js'
+import Notification from './components/error.js';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -11,6 +12,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState('');
 
   useEffect(() => {
     console.log('effect');
@@ -25,10 +27,20 @@ const App = () => {
       });
   }, [])
 
+  const handleNotification = (message) => {
+    setNotificationMessage(message);
+    setTimeout(() => setNotificationMessage(null), 3000);
+};
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter search={search} setSearch={setSearch} />
+      <Notification message={notificationMessage} />
+      <Filter 
+      search={search}
+      setSearch={setSearch} 
+      setNotificationMessage={handleNotification} 
+      />
       <h3>Add new</h3>
       <Form 
         newName={newName}
@@ -37,9 +49,15 @@ const App = () => {
         setNewNumber={setNewNumber}
         persons={persons}
         setPersons={setPersons}
+        setNotificationMessage={handleNotification} 
       />
       <h3>Numbers</h3>
-      <Numbers persons={persons} setPersons={setPersons} search={search}/>
+      <Numbers 
+      persons={persons}
+      setPersons={setPersons}
+      search={search}
+      setNotificationMessage={handleNotification} 
+      />
     </div>
   )
 }
