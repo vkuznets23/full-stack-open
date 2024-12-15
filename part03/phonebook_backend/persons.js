08d7361d50f0de6379804151e201a67ff6d1e975
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-12-15 18:59:31 +0200

    task 3.1

---
+++ part03/phonebook_backend/persons.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json()) //json-parser
+
+let contacts = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
+
+app.get('/api/persons', (request, resnonse) => {
+  resnonse.json(contacts)
+})
+
+const PORT = 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
