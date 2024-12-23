4669587fd55270761de906a5940ca382a15a9221
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-12-23 11:45:06 +0200

    front and back combined

--- part03/phonebook_backend/persons.js
+++ part03/phonebook_backend/persons.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.use(express.json()) //json-parser
 
+const cors = require('cors');
+
+// Enable CORS for all routes
+app.use(cors());
+
 morgan.token('jsonData', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :jsonData'));
 
@@ -55,9 +60,35 @@ app.get('/info', (req, res) => {
   `);
 })
 
+//UPDATE persons detail
+app.put('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const { name, number } = req.body
+
+  //validate data
+  if (!name || !number) {
+    return res.status(400).json ({
+      error: 'name and number are required'
+    });
+  }
+
+  // Check if contact exists
+  const contactIndex = contacts.filter(contact => contact.id === id)
+  if (contactIndex === -1) {
+    return res.status(404).json({ error: 'Contact not found.' });
+  }
+
+  // Update contact details
+  const updatedContact = { ...contacts[contactIndex], name, number };
+  contacts[contactIndex] = updatedContact;
+
+  res.json(updatedContact); // Send the updated contact back
+
+})
+
 //DELETE person by id
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
+  const id = Number(req.params.id)
   contacts = contacts.filter(contact => contact.id !== id)
   res.status(204).end()
 })
