9627166da6e5cf58771eb45011967f1507ffe035
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-01-22 19:14:57 +0200

    error msg inside popup

--- part03/phonebook_frontend/src/components/modal.js
+++ part03/phonebook_frontend/src/components/modal.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../styles/Modal.css";
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+  if (!isOpen) return null; // Render nothing if the modal is not open
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <button className="modal-close" onClick={onClose}>
-          &times;
-        </button>
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+      >
         {children}
       </div>
     </div>
