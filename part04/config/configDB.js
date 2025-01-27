ff962744090e3ac1ac5f3c4b79dda16c316e8492
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-01-27 21:42:27 +0200

    4.8-4.14

--- part04/config/configDB.js
+++ part04/config/configDB.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const password = process.env.MONGODB_PASSWORD;
-    if (!password) {
-    console.error('Please provide the MongoDB password in your .env file');
-    process.exit(1);
-    }
+    //const password = process.env.MONGODB_PASSWORD;
+    //if (!password) {
+    //console.error('Please provide the MongoDB password in your .env file');
+    //process.exit(1);
+    //}
 
-    const mongoUrl = `mongodb+srv://vkuznets:${password}@phonebook.3wnu5.mongodb.net/blogs?retryWrites=true&w=majority`;
+    const MONGODB_URI = process.env.NODE_ENV === 'test' 
+        ? process.env.TEST_MONGODB_URI
+        : process.env.MONGODB_URI
+
+    //const mongoUrl = `mongodb+srv://vkuznets:${password}@phonebook.3wnu5.mongodb.net/blogs?retryWrites=true&w=majority`;
 
     try {
-        await mongoose.connect(mongoUrl)
+        await mongoose.connect(MONGODB_URI)
         console.log('Connected to MongoDB')
     } catch(err) {
         console.error('Failed to connect to MongoDB:', err.message);
--- part04/index.js
+++ part04/index.js
@@ -22,3 +22,5 @@ const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = app;
