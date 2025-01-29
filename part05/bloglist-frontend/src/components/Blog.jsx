996fe5e0fb156ebb2e166276a17e5ec89c1e032d
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-01-29 15:42:14 +0200

    5.6

--- part05/bloglist-frontend/src/components/Blog.jsx
+++ part05/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from './Togglable';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <strong>{blog.title}</strong> {/* Always visible */}
+        <Togglable buttonLabel="View Details">
+          <p>by {blog.author}</p>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes || 0}</p>
+        </Togglable>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
