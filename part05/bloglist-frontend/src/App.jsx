e748e2c7a654ca3b5ce4e4f744b335c43ef88dd2
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-01-29 15:00:04 +0200

    5.5

--- part05/bloglist-frontend/src/App.jsx
+++ part05/bloglist-frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/login'
+import Toggable from './components/toggable'
 import blogService from './services/blogs'
 import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,12 +15,11 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [notification, setNotification] = useState(null)
   const [user, setUser] = useState(null)
-
-  const [newBlog, setNewBlog] = useState({
-    title: '',
-    author: '',
-    url: ''
-  })
+  
+  //new blog
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -26,7 +28,7 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
@@ -38,12 +40,8 @@ const App = () => {
     event.preventDefault();
 
     try {
-      const user = await loginService.login({
-        username, password,
-      })
-      window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      ) 
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
 
       blogService.setToken(user.token)
       setUser(user)
@@ -62,31 +60,6 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-          autoComplete="current-password"
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
-
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
     setUser(null)
@@ -95,26 +68,30 @@ const App = () => {
 
   const handleBlogChange = (event) => {
     const { name, value } = event.target
-    setNewBlog({ ...newBlog, [name]: value })
+    if (name === 'title') setTitle(value);
+    if (name === 'author') setAuthor(value);
+    if (name === 'url') setUrl(value);
   }
   
   const handleBlogSubmit = async (event) => {
     event.preventDefault()
 
-    if (!newBlog.title || !newBlog.author || !newBlog.url) {
+    if (!title || !author || !url) {
       setErrorMessage('All fields are required')
       setTimeout(() => setErrorMessage(null), 5000)
       return
     }
   
     try {
-      const createdBlog = await blogService.create(newBlog)
-      setBlogs(blogs.concat(createdBlog)) // Добавить новый блог в текущий список
-      setNotification(`blog ${newBlog.title} successfuly added`)
+      const createdBlog = await blogService.create({ title, author, url })
+      setBlogs(blogs.concat(createdBlog))
+      setNotification(`blog ${title} successfuly added`)
       setTimeout(() => {
         setNotification(null)
       }, 5000)
-      setNewBlog({ title: '', author: '', url: '' }) // Очистить форму
+      setTitle('');
+      setAuthor('');
+      setUrl('');
     } catch (error) {
       setErrorMessage('Failed to create blog')
       setTimeout(() => {
@@ -128,7 +105,7 @@ const App = () => {
       <div>
         <input
           type="text"
-          value={newBlog.title}
+          value={title}
           name="title"
           placeholder="Blog Title"
           onChange={handleBlogChange}
@@ -137,7 +114,7 @@ const App = () => {
       <div>
         <input
           type="text"
-          value={newBlog.author}
+          value={author}
           name="author"
           placeholder="Author"
           onChange={handleBlogChange}
@@ -146,7 +123,7 @@ const App = () => {
       <div>
         <input
           type="text"
-          value={newBlog.url}
+          value={url}
           name="url"
           placeholder="Blog URL"
           onChange={handleBlogChange}
@@ -164,16 +141,22 @@ const App = () => {
   
       {/* If the user is logged in, show blogs and user info */}
       {user === null ? (
-        <div>
-          <h2>Login</h2> {/* Login header */}
-          {loginForm()} {/* Render the login form */}
-        </div>
+        <Login
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          errorMessage={errorMessage}
+        />
       ) : (
         <div>
           <h2>Welcome, {user.name}!</h2> {/* Show the user's name */}
           <button onClick={handleLogout}>Logout</button>
           <h3>Add new blog</h3>
-          {blogForm()}
+          <Toggable>
+            {blogForm()}
+          </Toggable>
           <h3>Your Blogs</h3> {/* Section header for blogs */}
           {/* Display a logout button here if you want */}
           <div>
---
+++ part05/bloglist-frontend/src/components/login.jsx
@@ -0,0 +1,36 @@
+const Login = (props) => {
+  return (
+    <div>
+      {/* Display error message if login fails */}
+      {props.errorMessage && <div style={{ color: 'red' }}>{props.errorMessage}</div>}
+      <h2>Login</h2>
+      <form onSubmit={props.handleLogin}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={props.username}
+            name="Username"
+            onChange={({ target }) => props.setUsername(target.value)}
+            autoComplete="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={props.password}
+            name="Password"
+            onChange={({ target }) => props.setPassword(target.value)}
+            autoComplete="current-password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
