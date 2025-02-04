899e8bb2dfa84558b11aa08dc40f3a8c8c4c26d9
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-02-04 18:44:48 +0200

    remove the rest

--- part03/phonebook_backend/utils/logger.js
+++
@@ -1,5 +0,0 @@
-const morgan = require('morgan');
-morgan.token('jsonData', (req) => JSON.stringify(req.body));
-const logger = morgan(':method :url :status :response-time ms - :res[content-length] :jsonData');
-
-module.exports = logger;
