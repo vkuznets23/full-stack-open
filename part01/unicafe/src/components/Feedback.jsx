a20306628e5cf41ff1afb0fd1ffa74570f01a049
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-02-08 18:15:11 +0200

    1.8

--- part01/unicafe/src/components/Feedback.jsx
+++ part01/unicafe/src/components/Feedback.jsx
@@ -1,12 +1,38 @@
 import Button from './Button'
 
-const Feedback = ({ setBad, setGood, setNeutral, good, neutral, bad }) => {
+const Feedback = ({
+  setBad,
+  setGood,
+  setNeutral,
+  good,
+  neutral,
+  bad,
+  setAll,
+}) => {
+  const handleGood = () => {
+    const goodMark = good + 1
+    setGood(goodMark)
+    setAll(goodMark + bad + neutral)
+  }
+
+  const handleBad = () => {
+    const badMark = bad + 1
+    setBad(badMark)
+    setAll(good + badMark + neutral)
+  }
+
+  const handleNeutral = () => {
+    const neutralMark = neutral + 1
+    setNeutral(neutralMark)
+    setAll(good + bad + neutralMark)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
     </div>
   )
 }
--- part01/unicafe/src/components/Statistic.jsx
+++ part01/unicafe/src/components/Statistic.jsx
@@ -1,10 +1,21 @@
-const Statistic = ({ good, bad, neutral }) => {
+const Statistic = ({ good, bad, neutral, all }) => {
   return (
     <div>
       <h1>statistic</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      {all === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {all}</p>
+          <p>
+            Average: {all === 0 ? 0 : (good * 1 + neutral + bad * -1) / all}
+          </p>
+          <p>Positive: {all === 0 ? 0 : (good / all) * 100}%</p>
+        </>
+      )}
     </div>
   )
 }
