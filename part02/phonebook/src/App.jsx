473730aae8f926f695b1e7e5e6f5010e23eab7ce
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-02-16 18:40:12 +0200

    tasks till 2.17

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from 'react'
 import contactService from './services/service'
-import { AddContact, ContactList, Filter, Header, Loading } from './components'
+import {
+  AddContact,
+  ContactList,
+  Filter,
+  Header,
+  Loading,
+  Notification,
+} from './components'
 
 function App() {
   const [persons, setPersons] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [isToggled, setIsToggled] = useState(false)
+  const [notification, setNotification] = useState({
+    message: null,
+    type: null,
+  })
   const [search, setSearch] = useState('')
 
   useEffect(() => {
@@ -16,6 +27,7 @@ function App() {
         setIsLoading(false)
       } catch (error) {
         console.error('Failed to fetch contacts:', error)
+        setNotification({ message: 'Failed to fetch contacts', type: 'error' })
       }
     }
     fetchData()
@@ -43,15 +55,30 @@ function App() {
   return (
     <main>
       <div className="container">
+        <Notification
+          message={notification.message}
+          type={notification.type}
+          setMessage={(msg, type) => setNotification({ message: msg, type })}
+        />
         <Header
           title="Contacts"
           persons={persons}
           setIsToggled={setIsToggled}
           isToggled={isToggled}
         ></Header>
-        {isToggled && <AddContact persons={persons} setPersons={setPersons} />}
+        {isToggled && (
+          <AddContact
+            persons={persons}
+            setPersons={setPersons}
+            setNotification={setNotification}
+          />
+        )}
         <Filter search={search} setSearch={setSearch} />
-        <ContactList persons={getFilteredContacts()} setPersons={setPersons} />
+        <ContactList
+          persons={getFilteredContacts()}
+          setPersons={setPersons}
+          setNotification={setNotification}
+        />
       </div>
     </main>
   )
