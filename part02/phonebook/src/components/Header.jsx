b3f98edcc5cc24b96f5f8846144b4e884500adb0
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-02-16 17:01:24 +0200

    come changes

--- part02/phonebook/src/components/Header.jsx
+++ part02/phonebook/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import { MdAdd } from 'react-icons/md'
+import { IoCloseSharp } from 'react-icons/io5'
 
-const Header = ({ title, children }) => {
+const Header = ({ title, persons, isToggled, setIsToggled }) => {
   return (
     <div className="header">
       <div>
         <h1>{title}</h1>
-        {children}
+        <p style={{ paddingLeft: 5, paddingTop: 5 }}>
+          Total {persons.length} contacts
+        </p>
       </div>
-      <button className="add-button">
-        <MdAdd />
+      <button className="add-button" onClick={() => setIsToggled(!isToggled)}>
+        {isToggled ? <MdAdd /> : <IoCloseSharp />}
       </button>
     </div>
   )
