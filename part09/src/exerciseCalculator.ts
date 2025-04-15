afe3245cca917bfedaf804482f79d1af6a72aed9
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-15 11:07:59 +0300

    part9: ex till 9.7

--- part09/src/exerciseCalculator.ts
+++ part09/src/exerciseCalculator.ts
@@ -10,7 +10,7 @@ type returnValue = {
   ratingDescription: string
 }
 
-function calculateExercises(
+export function calculateExercises(
   dailyExerciseHours: number[],
   targetAmount: number
 ): returnValue {
@@ -47,35 +47,40 @@ function calculateExercises(
 }
 
 // console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
-const args2 = process.argv.slice(2)
+// const args2 = process.argv.slice(2)
+if (require.main === module) {
+  const args2 = process.argv.slice(2)
 
-if (args2.length < 2) {
-  throw new Error(
-    'Error: Please provide both daily exercise hours and target amount.'
-  )
-}
-
-// Convert the first argument (dailyExerciseHours) into an array of numbers
-const dailyExerciseHours = args2[0].split(',').map((str) => {
-  const num = Number(str.trim())
-  if (isNaN(num)) {
+  if (args2.length < 2) {
     throw new Error(
-      `Error: Invalid number in daily exercise hours: "${str.trim()}"`
+      'Error: Please provide both daily exercise hours and target amount.'
     )
   }
-  return num
-})
 
-const targetAmount = Number(args2[1])
+  // Convert the first argument (dailyExerciseHours) into an array of numbers
+  const dailyExerciseHours = args2[0].split(',').map((str) => {
+    const num = Number(str.trim())
+    if (isNaN(num)) {
+      throw new Error(
+        `Error: Invalid number in daily exercise hours: "${str.trim()}"`
+      )
+    }
+    return num
+  })
 
-if (isNaN(targetAmount)) {
-  throw new Error('Error: Please provide a valid number for the target amount.')
-}
+  const targetAmount = Number(args2[1])
 
-try {
-  console.log(calculateExercises(dailyExerciseHours, targetAmount))
-} catch (err) {
-  if (err instanceof Error) {
-    console.error(err.message)
+  if (isNaN(targetAmount)) {
+    throw new Error(
+      'Error: Please provide a valid number for the target amount.'
+    )
+  }
+
+  try {
+    console.log(calculateExercises(dailyExerciseHours, targetAmount))
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message)
+    }
   }
 }
--- part09/src/index.ts
+++ part09/src/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import { calculateBmi } from './bmiCalculator'
+import { calculateExercises } from './exerciseCalculator'
 
 const app = express()
 app.use(express.json())
@@ -8,24 +9,56 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
 })
 
-app.get('/bmi', (req: Request, res: Response) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
 
-  if (!height || !weight || isNaN(height) || isNaN(weight)) {
-    res.status(400).json({ error: 'malformatted parameters' })
-  }
   try {
-    const bmi = calculateBmi(height, weight)
-    res.json({
-      height: height,
-      weight: weight,
+    const heightValue: number = parseFloat(height as string)
+    const weightValue: number = parseFloat(weight as string)
+
+    if (isNaN(heightValue) || isNaN(weightValue)) {
+      throw Error()
+    }
+
+    const bmi: string = calculateBmi(heightValue, weightValue)
+    return res.json({
+      height: heightValue,
+      weight: weightValue,
       bmi: bmi,
     })
   } catch (err) {
-    if (err instanceof Error) res.status(400).json({ error: err.message })
+    if (err instanceof Error)
+      return res.status(404).json({
+        error: 'malformatted parameters',
+      })
   }
-  res.status(500).json({ error: 'Unknown error' })
+
+  return res.status(500).json({ error: 'Unexpected error' })
+})
+
+app.post('/exercises', (req, res) => {
+  const { daily_exercises, target } = req.body
+
+  if (!daily_exercises || !target) {
+    return res.status(400).json({
+      error: 'parameters missing',
+    })
+  }
+
+  if (
+    !Array.isArray(daily_exercises) ||
+    daily_exercises.some(isNaN) ||
+    isNaN(target as number)
+  ) {
+    return res.status(400).json({
+      error: 'malformatted parameters',
+    })
+  }
+  const result = calculateExercises(
+    daily_exercises as number[],
+    target as number
+  )
+  return res.json(result)
 })
 
 const PORT = 3003
