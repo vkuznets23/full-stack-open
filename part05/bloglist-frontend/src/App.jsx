6691ec1cdb34509260d8e55df79a628917b381e1
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-21 20:43:59 +0300

    task 5.10

--- part05/bloglist-frontend/src/App.jsx
+++ part05/bloglist-frontend/src/App.jsx
@@ -125,9 +125,11 @@ const App = () => {
         </>
       )}
       <h2>Blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </div>
   )
 }
