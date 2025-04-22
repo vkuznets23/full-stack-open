b20df6da11e0c247cf6d0319d8409faf2457276d
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-22 15:26:46 +0300

    5.17-5.18: reset fot testing db and loggin tests

--- part05/E2Etest/tests/blogapp.spec.js
+++ part05/E2Etest/tests/blogapp.spec.js
@@ -1,7 +1,18 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3002/api/testing/reset')
+
+    //create user
+    await request.post('http://localhost:3002/api/users', {
+      data: {
+        username: 'vikatest',
+        name: 'Vika Testova',
+        password: 'testpass',
+      },
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,12 +21,20 @@ describe('blog app', () => {
     await expect(locator).toBeVisible()
   })
 
-  test('logging in', async ({ page }) => {
+  test('successful logging in', async ({ page }) => {
     await page.getByRole('textbox').first().fill('vikatest')
     await page.getByRole('textbox').last().fill('testpass')
     await page.getByRole('button', { name: 'login' }).click()
 
-    await expect(page.getByText('You are logged in')).toBeVisible()
+    await expect(page.getByText('Vika Testova logged in')).toBeVisible()
     await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+  })
+
+  test('error logging in', async ({ page }) => {
+    await page.getByRole('textbox').first().fill('vikatest')
+    await page.getByRole('textbox').last().fill('wrongpass')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Wrong credentials')).toBeVisible()
   })
 })
