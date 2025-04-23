a157bffabb712de83b8f51d1150c2346454cc13e
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-23 15:11:16 +0300

    tasks 9.10-11

---
+++ part09/Patientor_server/src/routes/patients.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import patients from '../db/patients'
+import { NonSensitivePatient } from '../types/patients'
+
+const router = express.Router()
+
+router.get('/', (_req, res) => {
+  const nonSensitivePatients: NonSensitivePatient[] = patients.map(
+    ({ id, name, dateOfBirth, gender, occupation }) => ({
+      id,
+      name,
+      dateOfBirth,
+      gender,
+      occupation,
+    })
+  )
+  res.json(nonSensitivePatients)
+})
+
+export default router
