43fd103fe37fda1f87e87a9be20f224e52a73522
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-05-22 11:45:09 +0300

    9.23

--- part09/Patientor_server/src/types/patients.ts
+++ part09/Patientor_server/src/types/patients.ts
@@ -1,9 +1,50 @@
 import { z } from 'zod'
 import { newEntrySchema } from '../utils'
+import { Diagnosis } from './diagnosis'
 
 export type NewPatient = z.infer<typeof newEntrySchema>
 
-export interface Entry {}
+interface BaseEntry {
+  id: string
+  date: string
+  description: string
+  specialist: string
+  diagnosisCodes?: Diagnosis['code'][]
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck'
+  healthCheckRating: HealthCheckRating
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital'
+  discharge: {
+    date: string
+    criteria: string
+  }
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare'
+  employerName: string
+  sickLeave?: {
+    startDate: string
+    endDate: string
+  }
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry
 
 export interface Patient extends NewPatient {
   id: string
